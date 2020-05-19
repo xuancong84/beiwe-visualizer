2f7069823d7aabd9a061a64eac58e915b42b2a11
xuancong84 <xuancong84@gmail.com>
2020-05-19 18:47:45 +0800

    added Shap plot

--- compare-CB.py
+++ compare-CB.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import os, sys, matplotlib, re, io, traceback, math, argparse
+import os, sys, re, io, math, argparse
+import matplotlib, shap, xgboost
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -44,10 +45,14 @@ def summarize(df):
 
 ## MAIN
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser(usage='$0 [options] <input 1>output 2>progress', description='compare data before and after circuit breaker')
+	parser = argparse.ArgumentParser(usage='$0 [options] <input 1>output 2>progress',
+	                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+	                                 description='compare data before and after circuit breaker')
 	parser.add_argument('--input-file', '-i', help='input file, default="-" (STDIN)', type=str, default='-')
+	parser.add_argument('--output-file', '-o', help='output file, default="-" (STDOUT)', type=str, default='-')
 	parser.add_argument('--CB-start-date', '-d', help='date when circuit breaker occurred', type=str, default='2020-04-07')
 	parser.add_argument('--CB-boundary-gap', '-g', help='gap in days before and after CB-start-date', type=str, default='7D')
+	parser.add_argument('--save-shap', '-s', help='save Shap plot to file', type=str, default='')
 	opt = parser.parse_args()
 	globals().update(vars(opt))
 
@@ -57,4 +62,25 @@ if __name__ == '__main__':
 	all_data = pandas_load(input_file)
 	dfs = pd.concat([compare_stats(summarize(df)[0]) for p, df in all_data.items()])
 	df_compare = dfs.groupby(dfs.index).mean()
-	df_compare.to_csv(sys.stdout)
+	df_compare.to_csv(Open(output_file, 'w'))
+
+	if save_shap:
+		df = pd.concat([summarize(df)[0] for p, df in all_data.items()])
+		df_before = df[df.index < CB_start_date - CB_boundary_gap]
+		df_after = df[df.index > CB_start_date + CB_boundary_gap]
+		X = pd.concat([df_before, df_after])
+		Y = pd.Series([-1] * len(df_before.index) + [1] * len(df_after.index))
+
+		# Build the model
+		if True:
+			# use xgboost algorithm
+			model = xgboost.train({"learning_rate": 0.01}, xgboost.DMatrix(X, label=Y), 100)
+		else:
+			# use random forest regression algorithm
+			model = RandomForestRegressor(max_depth=6, random_state=0, n_estimators=10)
+			model.fit(X, Y)
+
+		shap_values = shap.TreeExplainer(model).shap_values(X)
+		f = plt.figure()
+		shap.summary_plot(shap_values, X, max_display=9999)
+		f.savefig(save_shap, bbox_inches='tight', dpi=900)
