b3f830ebf120e90b0c0e0b506821f56ce2cc9a8a
Wang Xuancong <xuancong84@gmail.com>
2023-02-13 15:17:22 +0800

    added check-compliance.py

---
+++ check-compliance.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# coding=utf-8
+
+import os, sys, argparse, re, gzip
+import pandas as pd
+from glob import glob
+from utils1 import *
+from tqdm import tqdm
+
+
+def comp_compliance(dir_bn):
+	bn = os.path.basename(dir_bn)
+	un = bn.split('_')[0]
+	if un not in df_master.index:
+		return 0.0
+	enrol_date = df_master.loc[un, 'enrol_date']
+	start_date = pd.to_datetime(enrol_date).tz_localize('tzlocal()') + pd.to_timedelta('1D')
+	end_date = min(start_date+pd.to_timedelta(duration), pd.Timestamp.now('tzlocal()').floor('D'))
+	denom = (end_date-start_date)/pd.to_timedelta('1H')
+	end_date -= pd.to_timedelta('1s')
+	if not os.path.exists(dir_bn+'/ambientLight.csv.gz'):
+		sc1 = 0.0
+	else:
+		df1 = load_and_preprocess(dir_bn+'/ambientLight.csv.gz')['value']
+		sr1 = (df1.groupby(pd.Grouper(freq='1H')).count()>0)[start_date:end_date]
+		sc1 = sr1.sum()/denom
+	if not os.path.exists(dir_bn+'/heart.csv.gz'):
+		sc2 = 0.0
+	else:
+		df2 = load_and_preprocess(dir_bn+'/heart.csv.gz')['HR']
+		sr2 = (df2.groupby(pd.Grouper(freq='1H')).count()>0)[start_date:end_date]
+		sc2 = sr2.sum()/denom
+	return (sc1+sc2)*.5
+
+
+if __name__=='__main__':
+	parser = argparse.ArgumentParser(usage='$0 arg1 1>output 2>progress', description='compute compliance rate by looking at hourly presence of ambient light and heart rate',
+			formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('master_csv', help="the master CSV file")
+	parser.add_argument('in_dirs', help="wildcard of patients' directories", nargs = '+')
+	parser.add_argument('--duration', '-dur', help='optional argument', default = '7D')
+	parser.add_argument('-optional', help='optional argument')
+	#nargs='?': optional positional argument; action='append': multiple instances of the arg; type=; default=
+	opt=parser.parse_args()
+	globals().update(vars(opt))
+
+	df_master = pd.read_csv(master_csv).set_index('user_id')
+
+	out = {}
+	paths = [path for patn in in_dirs for path in glob(patn)]
+	for path in tqdm(paths):
+		dir_bn = path.rstrip('/')
+		out[os.path.basename(dir_bn)] = comp_compliance(dir_bn)
+
+	print(pd.Series(out).to_csv())
--- utils1.py
+++ utils1.py
@@ -1,85 +1,109 @@
 import seaborn as sns
 import matplotlib.pyplot as plt
 import pandas as pd
-import os
+import os, sys, gzip
 
-def convert_timestamp(timestamp, zone='UTC'):
-    datetime = pd.to_datetime(timestamp, unit='ms', origin='unix')
-    if zone is'SG':
-        datetime = datetime.tz_localize('UTC').tz_convert('Asia/Singapore')
-    
-    return datetime
+
+def Open(fn, mode = 'r', **kwargs):
+	if fn == '-':
+		return sys.stdin if mode.startswith('r') else sys.stdout
+	fn = os.path.expanduser(fn)
+	return gzip.open(fn, mode, **kwargs) if fn.lower().endswith('.gz') else open(fn, mode, **kwargs)
+
+
+def convert_timestamp(timestamp, zone = 'UTC'):
+	datetime = pd.to_datetime(timestamp, unit = 'ms', origin = 'unix')
+	if zone == 'SG':
+		datetime = datetime.tz_localize('UTC').tz_convert('Asia/Singapore')
+	return datetime
 
 
 def localize_date(date):
-    datetime = pd.to_datetime(date)
+	datetime = pd.to_datetime(date)
 
-    return datetime.tz_localize('UTC').tz_convert('Asia/Singapore')
+	return datetime.tz_localize('UTC').tz_convert('Asia/Singapore')
 
 
 def create_df(path, directory):
-    df_raw = pd.DataFrame({})
-    dir_path = os.path.join(path, directory)
-    for file in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, file)
-        df_raw = pd.concat([df_raw, pd.read_csv(file_path, error_bad_lines=False)])
-    
-    df = pd.DataFrame({})
-    if 'timestamp' in df_raw.columns:
-        
-        df = df_raw[df_raw.timestamp != 'timestamp']
-        df['timestamp'] = df['timestamp'].astype('str')
-#         df = df[df['timestamp'].str.len() == 13]
-        df['datetime'] = df.timestamp.apply(convert_timestamp, zone='SG')
-        df['day_of_week'] = df.datetime.dt.dayofweek
-        df['weekday_name'] = df.datetime.dt.weekday_name
-        df['hour'] = df.datetime.dt.hour
-    
-    if directory == 'tapsLog':
-        df.fillna(method='ffill', inplace=True)
+	df_raw = pd.DataFrame({})
+	dir_path = os.path.join(path, directory)
+	for file in os.listdir(dir_path):
+		file_path = os.path.join(dir_path, file)
+		df_raw = pd.concat([df_raw, pd.read_csv(file_path, error_bad_lines = False)])
 
-    return df
+	df = pd.DataFrame({})
+	if 'timestamp' in df_raw.columns:
+		df = df_raw[df_raw.timestamp != 'timestamp']
+		df['timestamp'] = df['timestamp'].astype('str')
+		#         df = df[df['timestamp'].str.len() == 13]
+		df['datetime'] = df.timestamp.apply(convert_timestamp, zone = 'SG')
+		df['day_of_week'] = df.datetime.dt.dayofweek
+		df['weekday_name'] = df.datetime.dt.weekday_name
+		df['hour'] = df.datetime.dt.hour
 
-def plot_measurement_heatmap(df, cat, norm=False):
-    plot_data = df.groupby(['day_of_week', 'hour']).size().reset_index(name='count')
-    if norm:
-        plot_data['count'] = (plot_data['count'] - plot_data['count'].mean()) / plot_data['count'].std()
+	if directory == 'tapsLog':
+		df.fillna(method = 'ffill', inplace = True)
 
-    fig, ax = plt.subplots(figsize=(10, 10))
+	return df
 
-    piv = pd.pivot_table(plot_data, values='count',index=['hour'], columns=['day_of_week'], fill_value=0)
-    ax = sns.heatmap(piv, square=True, cmap="BuPu")
-    ax.invert_yaxis()
-    plt.tight_layout()
-    plt.title(cat)
-    
-    
+
+def plot_measurement_heatmap(df, cat, norm = False):
+	plot_data = df.groupby(['day_of_week', 'hour']).size().reset_index(name = 'count')
+	if norm:
+		plot_data['count'] = (plot_data['count'] - plot_data['count'].mean()) / plot_data['count'].std()
+
+	fig, ax = plt.subplots(figsize = (10, 10))
+
+	piv = pd.pivot_table(plot_data, values = 'count', index = ['hour'], columns = ['day_of_week'], fill_value = 0)
+	ax = sns.heatmap(piv, square = True, cmap = "BuPu")
+	ax.invert_yaxis()
+	plt.tight_layout()
+	plt.title(cat)
+
+
 def plot_apps_usage(taps_data, start_date, end_date):
-    app_taps_activity = taps_data['in_app_name'].value_counts()
-    android_apps = app_taps_activity.index.str.contains('android')
-    no_android_apps = app_taps_activity[~android_apps]
-    no_android_apps.index.name = 'Apps'
-    no_android_apps = no_android_apps.reset_index(name='Taps')
-    
-    f, ax = plt.subplots(figsize=(9,8))
-    sns.barplot(x='Taps', y='Apps', data=no_android_apps, orient='h')
-    plt.yticks(fontsize=14)
-    plt.title(f'{start_date.date()} to {end_date.date()}')
-    
+	app_taps_activity = taps_data['in_app_name'].value_counts()
+	android_apps = app_taps_activity.index.str.contains('android')
+	no_android_apps = app_taps_activity[~android_apps]
+	no_android_apps.index.name = 'Apps'
+	no_android_apps = no_android_apps.reset_index(name = 'Taps')
+
+	f, ax = plt.subplots(figsize = (9, 8))
+	sns.barplot(x = 'Taps', y = 'Apps', data = no_android_apps, orient = 'h')
+	plt.yticks(fontsize = 14)
+	plt.title(f'{start_date.date()} to {end_date.date()}')
+
+
 def gps_heatmap(x, y):
-    '''
-    The point intensity corresponds to the times a place is visited.
-    '''
-    from scipy.stats import gaussian_kde
-    import numpy as np
-    
-    xy = np.vstack([x,y])
-    z = gaussian_kde(xy)(xy)
+	'''
+	The point intensity corresponds to the times a place is visited.
+	'''
+	from scipy.stats import gaussian_kde
+	import numpy as np
 
-    fig, ax = plt.subplots()
-    fig.set_size_inches(10, 7)
+	xy = np.vstack([x, y])
+	z = gaussian_kde(xy)(xy)
 
-    ax.scatter(x, y, c=z, s=z*0.5, edgecolor='')
+	fig, ax = plt.subplots()
+	fig.set_size_inches(10, 7)
 
-    plt.plot(x,y,c='purple', linewidth=0.8)
-    plt.show()
+	ax.scatter(x, y, c = z, s = z * 0.5, edgecolor = '')
+
+	plt.plot(x, y, c = 'purple', linewidth = 0.8)
+	plt.show()
+
+
+def preprocess_df(df):
+	# convert timestamp to datetime and set as index
+	dt = pd.to_datetime(df['timestamp'], unit = 'ms', origin = 'unix', utc = True)
+	df = df.set_index(pd.DatetimeIndex(dt).tz_convert('tzlocal()')).sort_index()
+
+	# remove rows with duplicate timestamp
+	df = df.loc[~df.index.duplicated(keep = 'last')]
+	df.index.name = 'datetime'
+	return df
+
+
+def load_and_preprocess(fn):
+	df = pd.read_csv(Open(fn))
+	return preprocess_df(df)
